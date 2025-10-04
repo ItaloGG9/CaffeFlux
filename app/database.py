7c5711c51d37f09f1ddd219b59db34e7f452490b
ItaloGG9 <104172674+ItaloGG9@users.noreply.github.com>
2025-10-04 17:56:53 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:toBqhBcHNmbrhvsKWbtbCliyxxHXetWo@postgres.railway.internal:5432/railway')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:1123@postgres.railway.internal:5432/railway')
 
 # engine with future flag for SQLAlchemy 2
 engine = create_engine(DATABASE_URL, future=True)
