03d1d45ce6b3905b11982c7f1efb0ceba05ce091
ItaloGG9 <104172674+ItaloGG9@users.noreply.github.com>
2025-10-07 15:54:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI
+from app.database import init_db  # porque database.py está dentro de /app
 from dotenv import load_dotenv
 from app.database import engine, Base
 from app.routers import products, jerarquia, mesas, pedidos, turnos, pagos
