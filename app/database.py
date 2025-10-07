1f7cd2c3d7d63fc742eb83acc8d531c3f9179f4f
ItaloGG9 <104172674+ItaloGG9@users.noreply.github.com>
2025-10-07 15:54:40 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,8 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:1123@postgres.railway.internal:5432/railway')
+DATABASE_URL = os.getenv(
+    'DATABASE_URL',
+    'postgresql://postgres:1123@postgres.railway.internal:5432/railway'
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def init_db():
+    import app.models  # Importar modelos para crear tablas
+    Base.metadata.create_all(bind=engine)
